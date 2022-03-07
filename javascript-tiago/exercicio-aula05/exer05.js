ef6f4c51ef9e076b1e41fd30428ea2a283358c32
gaabrielgr <gabrielgr.ti@gmail.com>
2022-03-07 19:11:06 -0300

    refatorando o código do exercicio06

--- javascript-tiago/exercicio-aula05/exer05.js
+++ javascript-tiago/exercicio-aula05/exer05.js
@@ -3,7 +3,7 @@ let nota02 = parseFloat(prompt("Insira o valor da segunda nota: "));
 let nota03 = parseFloat(prompt("Insira o valor da terceira nota: "));
 let media = (nota01 + nota02 + nota03) / 3;
 
-if (media > 7) {
+if (media >= 7) {
   console.log(`Parabéns sua média foi ${media.toFixed(2)}`);
 } else if (media >= 5 && media < 7) {
   console.log(`Você ficou em recuperação, sua média foi ${media.toFixed(2)}`);
--- javascript-tiago/exercicio-aula06/exer06.js
+++ javascript-tiago/exercicio-aula06/exer06.js
@@ -4,20 +4,28 @@ alert(
 
 let opcoesMenu = parseInt(prompt("Digite a opção desejada"));
 if (!isNaN(opcoesMenu) && opcoesMenu > 0 && opcoesMenu <= 3) {
-  if (opcoesMenu === 1) {
-    alert("Seja bem-vindo!");
-  }
-  if (opcoesMenu === 2) {
-    let confirmacao = confirm("Tem certeza que deseja efetuar o checkout? ");
-    if (confirmacao) {
-      alert("Até a próxima");
-    }
-  }
-  if (opcoesMenu === 3) {
-    let maisDias = prompt("Quantos dias a mais vocÊ gostaria de ficar? ");
-    if (!isNaN(maisDias) && maisDias >= 1) {
-      alert(`Parabéns, sua estadia foi estendida por ${maisDias} dias `);
-    }
+  switch (opcoesMenu) {
+    case 1:
+      alert("Seja bem-vindo!");
+      break;
+    case 2:
+      let confirmacao = confirm("Tem certeza que deseja efetuar o checkout?");
+      if (confirmacao === true) {
+        alert("Até a próxima");
+      } else {
+        console.log("Volte ao menu para realizar sua operação!");
+      }
+      break;
+    case 3:
+      let dias = prompt("Quantos dias você deseja ficar a mais?");
+      if (!isNaN(dias) && dias > 0) {
+        alert(`Parabéns, Sua estadia foi estendida por ${dias} dias`);
+      } else {
+        alert(
+          "Que pena, não conseguimos estender sua hospedagem. Lembre-se de digitar um número positivo."
+        );
+      }
+      break;
   }
 } else {
   console.log("digite um número válido entre 1 e 3");
