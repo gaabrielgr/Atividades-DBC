e135ae660df4120caf7343f7b136c13c03dbc870
gaabrielgr <gabrielgr.ti@gmail.com>
2022-03-31 01:06:44 -0300

    layout pagina address

--- react/login/src/pages/Address.js
+++ react/login/src/pages/Address.js
@@ -1,25 +1,50 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import styleAddress from "../components/Address.module.css";
 import ApiCep from "./ApiCep";
-import { useForm } from "react-hook-form";
-
 function Address() {
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, setValue } = useForm();
   const [cepApi, setCepApi] = useState({});
+  const [cep, setCep] = useState("");
+  const [error, setError] = useState(null);
 
-  function onSubmit(e) {
-    console.log(e);
+  function handleChange({ target }) {
+    if (error) validateCep(target.value);
+    setCep(target.value);
   }
-  const ValidarCep = async (e) => {
-    const cepInformado = e.target.value.replace(/\D/g, "");
-    console.log(cepInformado);
-    try {
-      const { data } = await ApiCep.get(`/${cepInformado}/json/`);
-      setCepApi(data);
-    } catch (error) {
-      console.log("Falha ao fazer a requisição", error);
+
+  function handleBlur({ target }) {
+    validateCep(target.value);
+  }
+  async function validateCep(value) {
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError("Preencha um cep válido");
+      return false;
+    } else {
+      try {
+        const { data } = await ApiCep.get(`/${value}/json/`);
+        setCepApi(data);
+        console.log(data);
+      } catch (error) {
+        console.log("Falha ao fazer a requisição", error);
+      }
+      setError(null);
+      return true;
     }
-  };
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    if (validateCep(cep)) {
+      console.table(cepApi);
+    } else {
+      console.log("Não enviar");
+    }
+  }
+
   setValue("logradouro", cepApi.logradouro);
   setValue("bairro", cepApi.bairro);
   setValue("cidade", cepApi.localidade);
@@ -28,31 +53,61 @@ function Address() {
   setValue("cep", cepApi.cep);
 
   return (
-    <div>
-      <h1>Digite o seu CEP</h1>
+    <div className={styleAddress.divContainer}>
+      <form className={styleAddress.endereco} onSubmit={onSubmit}>
+        <h1>Digite o seu CEP</h1>
+        {error && <p className={styleAddress.error}>{error}</p>}
+        <div className={styleAddress.divInput}>
+          <label htmlFor="cep">CEP</label>
+          <input
+            {...register("cep")}
+            label="CEP"
+            id="cep"
+            type="text"
+            value={cep}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
 
-      <form className={styleAddress.endereco} onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="cep">CEP</label>
-        <input
-          {...register("cep")}
-          id="cep"
-          name="cep"
-          placeholder="Digite seu CEP"
-          onBlur={ValidarCep}
-        />
-        <label htmlFor="rua">Rua</label>
-        <input id="rua" type="text" {...register("logradouro")} />
+        <div className={styleAddress.divInput}>
+          <label htmlFor="rua">Rua</label>
+          <input id="rua" type="text" {...register("logradouro")} />
+        </div>
         <label htmlFor="bairro">Bairro</label>
         <input id="bairro" name="bairro" type="text" {...register("bairro")} />
-        <label htmlFor="cidade">Cidade</label>
-        <input id="cidade" name="cidade" type="text" {...register("cidade")} />
-        <label htmlFor="estado">Estado</label>
-        <input id="estado" name="estado" type="text" {...register("estado")} />
-        <label htmlFor="dd">DD</label>
-        <input id="dd" name="dd" type="text" {...register("dd")} />
-        <label htmlFor="telefone">Telefone</label>
-        <input id="telefone" name="telefone" type="text" />
-        <button type="submit">Cadastrar</button>
+        <div className={styleAddress.divInput}>
+          <label htmlFor="cidade">Cidade</label>
+          <input
+            id="cidade"
+            name="cidade"
+            type="text"
+            {...register("cidade")}
+          />
+        </div>
+        <div className={styleAddress.divInput}>
+          <label htmlFor="estado">Estado</label>
+          <input
+            id="estado"
+            name="estado"
+            type="text"
+            {...register("estado")}
+          />
+        </div>
+        <div className={styleAddress.divInput}>
+          <label htmlFor="dd">DD</label>
+          <input id="dd" name="dd" type="text" {...register("dd")} />
+        </div>
+        <div className={styleAddress.divInput}>
+          <label htmlFor="numero">N°</label>
+          <input
+            id="numero"
+            name="numero"
+            type="text"
+            {...register("numero")}
+          />
+        </div>
+        <button>Cadastrar</button>
       </form>
     </div>
   );
