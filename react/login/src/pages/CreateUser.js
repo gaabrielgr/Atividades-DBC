367c21c6b1576a2cc328720435e89c8f9d7882a6
gaabrielgr <gabrielgr.ti@gmail.com>
2022-04-01 01:32:02 -0300

    tentando validar onchange com formik

--- react/login/src/pages/CreateUser.js
+++ react/login/src/pages/CreateUser.js
@@ -1,25 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Formik, Field, Form } from "formik";
 import { ToastContainer, toast } from "react-toastify";
-import { useLocation } from "react-router-dom";
+import { matchPath, useLocation } from "react-router-dom";
 
 import "react-toastify/dist/ReactToastify.css";
 import api from "../api";
 
 const CreateUser = () => {
-  const [attUserList, setAttUserList] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [attUserList, setAttUserList] = useState({});
+  const [changeButton, setChangeButton] = useState(true);
   const location = useLocation();
   const idUserAtt = location.pathname.split("/create-user/")[1];
+
   const notify = () => {
     toast.success("Usuário cadastrado com sucesso !", {
       position: toast.POSITION.TOP_CENTER,
     });
   };
+  const notifyAtt = () => {
+    toast.info("Usuário atualizado com sucesso !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
   async function CreateNewUser(values) {
-    console.log(values);
     try {
       const { data } = await api.post("/pessoa", values);
+      console.log(data);
+      setChangeButton(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function attUser(values) {
+    /*  const user = {
+      cpf: values.cpf,
+      dataNascimento: values.dataNascimento,
+      email: values.email,
+      nome: values.nome,
+    }; */
+
+    try {
+      const { data } = await api.put(`/pessoa/${idUserAtt}`, values);
       console.log(data);
     } catch (error) {
       console.log(error);
@@ -32,18 +53,19 @@ const CreateUser = () => {
         `/pessoa/{idPessoa}?idPessoa=${idUserAtt}`
       );
       setAttUserList(data);
-      console.log(data);
+      setChangeButton(false);
     } catch (error) {
       console.log(error);
     }
   }
+
   useEffect(() => {
     if (idUserAtt.length > 0) {
       HandleGetUserAtt();
       return;
     }
   }, []);
-  console.log(attUserList);
+
   return (
     <Formik
       initialValues={{
@@ -53,27 +75,55 @@ const CreateUser = () => {
         nome: "",
       }}
       onSubmit={async (values) => {
-        CreateNewUser(values);
+        if (changeButton) {
+          CreateNewUser(values);
+        } else {
+          attUser(values);
+        }
       }}
     >
       <Form>
         <label htmlFor="nome">Nome</label>
-        <Field id="nome" name="nome" placeholder="Digite seu nome: " />
+        <Field
+          id="nome"
+          name="nome"
+          placeholder="Digite seu nome: "
+          value={attUserList.nome}
+        />
 
         <label htmlFor="email">Email</label>
-        <Field id="email" name="email" placeholder="email" type="email" />
+        <Field
+          id="email"
+          name="email"
+          placeholder="email"
+          type="email"
+          value={attUserList.email}
+        />
 
         <label htmlFor="dataNascimento">Data de nascimento</label>
         <Field
           id="dataNascimento"
           name="dataNascimento"
           placeholder="00/00/0000"
+          value={attUserList.dataNascimento}
         />
         <label htmlFor="cpf">CPF</label>
-        <Field id="cpf" name="cpf" placeholder="Digite seu CPF: " />
-        <button onClick={notify} type="submit">
-          Cadastrar
-        </button>
+        <Field
+          id="cpf"
+          name="cpf"
+          value={attUserList.cpf}
+          placeholder="Digite seu CPF: "
+        />
+        {changeButton ? (
+          <button onClick={notify} type="submit">
+            Cadastrar
+          </button>
+        ) : (
+          <button onClick={notifyAtt} type="submit">
+            Atualizar
+          </button>
+        )}
+
         <ToastContainer />
       </Form>
     </Formik>
